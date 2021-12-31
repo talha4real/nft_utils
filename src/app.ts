f020d8ca3dbc3732c7b5027821fd713d9af90cb0
talha4real <talhabutt993@gmail.com>
2022-01-01 00:27:42 +0500

    added prot

--- src/app.ts
+++ src/app.ts
@@ -24,5 +24,6 @@ app.get('/',async(req,res)=>{
     })()
 })
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5001,()=>console.log("Server running"))
+app.listen(PORT,()=>console.log("Server running"))
