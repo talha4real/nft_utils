615b54662cada0794a2b33de06075865a45a9c74
talha4real <talhabutt993@gmail.com>
2022-01-14 00:13:53 +0500

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
+const fs = require('fs');
 import express from 'express';
 const Moralis = require('moralis/node');
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -11,19 +11,37 @@ app.use(express.json());
 app.use(cors());
 
 
-
-
-
-app.get('/getOwnedNfts',async(req,res)=>{
+app.post('/getOwnedNfts',async(req:any,res:any)=>{
     (async()=>{
+        try{
+        const network = req.body.network;
+        const pubKey = req.body.public_key;
         console.log(`hello world`);
         Moralis.start({ serverUrl: "https://inuchpvr19tw.usemoralis.com:2053/server", appId: "MhvbvOiMJHtoL1iOTay1RS0wfwkfMRzzAj7kGbq8" });
-        const options = { chain: 'polygon', address: '0x939A2d5A8CB10afD30b4162F4b5885A3A547c225' };
-        const polygonNFTs = await Moralis.Web3API.account.getNFTs(options);
-        console.log(polygonNFTs);
+        const options = { chain: network, address: pubKey };
+        const retrieved_nfts = await Moralis.Web3API.account.getNFTs(options);
+        console.log(retrieved_nfts.result);
+        let result = retrieved_nfts.result;
+        await result.forEach(async(nft:any)=>{
+            try{
+                    await axios.get(nft.token_uri).then(res=>{
+                        // console.log(res)
+                        nft.metadata = res.data;
+                })
+                }catch(e){
+                    console.log("failed");
+                    nft.metadata ="not found";
+                }
+        })
+        // console.log(result);
+        await res.send({result: result})
+        }catch(e){
+            console.log(e);
+            res.send({failed: e})
+        }
     })()
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT=5005;
 
 app.listen(PORT,()=>console.log("Server running"))
